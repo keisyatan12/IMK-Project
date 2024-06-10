cf4ba4843b515f27ee70a804d3d1865b4a710751
ceycylia <ceycilia.dear3004@gmail.com>
2024-06-10 09:08:20 +0700

    Job Seeker update

--- resources/views/JobSeeker/jobseeker-alljob.blade.php
+++ resources/views/JobSeeker/jobseeker-alljob.blade.php
@@ -8,7 +8,7 @@
     @vite('resources/css/output.css')
     <title>{{ $page['title'] }}</title>
 </head>
-<body>
+<body x-data="{ open: false }">
 
 @include('partials/header')
 
@@ -17,21 +17,21 @@
 <div class="flex my-4 items-start">
     {{-- INI SIDEBAR --}}
     <div class="w-28 md:w-80 sidebar h-full bg-yellow-200 rounded-xl md:py-8 py-2 px-2 md:px-4 gap-4">
-        <a href="jobseeker-dashboard" class=" flex items-center gap-3 px-4 py-2">
+        <a href={{route('JobSeeker.index')}} class=" flex items-center gap-3 px-4 py-2">
             <img src="{{ asset('images/image-3.svg') }}" alt="home" />
             <span class="text-black text-base hidden md:block">Dashboard</span>
         </a>
-        <a href="jobseeker-alljob" class="bg-yellow-300  flex items-center gap-3 px-4 py-2">
+        <a href={{route('JobSeeker.alljob')}} class="bg-yellow-300  flex items-center gap-3 px-4 py-2">
             <img src="{{ asset('images/image-4.svg') }}" alt="user management" />
             <span class="text-black text-base hidden md:block">All Job</span>
         </a>
-        <a href="jobseeker-history" class="flex items-center gap-3 px-4 py-2">
+        <a href={{route('JobSeeker.history')}} class="flex items-center gap-3 px-4 py-2">
             <img src="{{ asset('images/image-6.svg') }}" alt="history" />
             <span class="text-black text-base hidden md:block">Application History</span>
         </a>
 
 
-        <a href="#" class="flex items-center gap-3 px-4 py-2">
+        <a href={{route('JobSeeker.profil')}} class="flex items-center gap-3 px-4 py-2">
             <img src="{{ asset('images/image-8.svg') }}" alt="home" />
             <span class="text-black text-base hidden md:block">Profile</span>
         </a>
@@ -47,17 +47,99 @@
 
         <div class="my-4 w-full mr-10 lg:mr-20 overflow-auto min-h-screen">
             {{-- INI BODY --}}
-            <div class="grid grid-cols-3 h-full">
-                <div class="col-span-2 justify-center flex">
-                    <div class="flex flex-col gap-8">
+            <div class="flex flex-col gap-4 h-full">
+                <div class="flex justify-center my-3">
 
-                        <span class="text-center">
-                            </span>
+                    <p class="text-3xl font-bold">Karir Terbaru Untuk Menjadi Tutor Di Seluruh Indonesia</p>
+                </div>
+                <div class="flex justify-center bg-white">
+
+                    <div class="grid items-center grid-cols-3 gap-2 px-4 py-2 bg-slate-200 rounded-full">
+                        <input class="w-full text-sm text-gray-700 bg-transparent border-none focus:border-none focus:ring-0 dark:text-white" placeholder="Search..." />
+                        <p class="text-end">|</p>
+                        <button type="button" class="p-2 bg-sky-500 text-white rounded-full">Search</button>
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-6 md:grid-cols-3 p-2 gap-6 bg-gray-200 rounded-md">
+                    {{-- card --}}
+                    <div class="flex flex-col gap-4 p-4 rounded-lg bg-white drop-shadow-lg ">
                         <span>
-
+                            <img src="{{asset('images/tutor.jpeg')}}" alt="karir-1" height="25%">
+                        </span>
+                        <span>
+                            <p class="font-bold text-lg">Matematic for 3 SMP</p>
+                            <p class="text-sm">09.00 - 12.00</p>
+                            <p class="text-sm">Rp. 750.000,00-/Month</p>
+                        </span>
+                        <span class="flex flex-row-reverse gap-4">
+                            <button class="px-4 py-2 rounded-lg bg-cyan-500 text-white drop-shadow-md">
+                                Apply Job
+                            </button>
+                            <button @click="open = true" class="px-4 py-2 rounded-lg bg-white drop-shadow-md">
+                                Detail Job
+                            </button>
+                        </span>
+                    </div>
+                    {{-- card --}}
+                    <div class="flex flex-col gap-4 p-4 rounded-lg bg-white drop-shadow-lg ">
+                        <span>
+                            <img src="#" alt="karir-1" height="50%" width="50%" >
+                        </span>
+                        <span>
+                            <p class="font-bold text-lg">Matematic for 3 SMP</p>
+                            <p class="text-sm">09.00 - 12.00</p>
+                            <p class="text-sm">Rp. 750.000,00-/Month</p>
+                        </span>
+                        <span class="flex flex-row-reverse gap-4">
+                            <button class="px-4 py-2 rounded-lg bg-cyan-500 text-white drop-shadow-md">
+                                Apply Job
+                            </button>
+                            <button class="px-4 py-2 rounded-lg bg-white drop-shadow-md">
+                                Detail Job
+                            </button>
+                        </span>
+                    </div>
+                    {{-- card --}}
+                    <div class="flex flex-col gap-4 p-4 rounded-lg bg-white drop-shadow-lg">
+                        <span>
+                            <img src="#" alt="karir-1" height="50%" width="50%" >
+                        </span>
+                        <span>
+                            <p class="font-bold text-lg">Matematic for 3 SMP</p>
+                            <p class="text-sm">09.00 - 12.00</p>
+                            <p class="text-sm">Rp. 750.000,00-/Month</p>
+                        </span>
+                        <span class="flex flex-row-reverse gap-4">
+                            <button class="px-4 py-2 rounded-lg bg-cyan-500 text-white drop-shadow-md">
+                                Apply Job
+                            </button>
+                            <button  class="px-4 py-2 rounded-lg bg-white drop-shadow-md">
+                                Detail Job
+                            </button>
+                        </span>
+                    </div>
+                    {{-- card --}}
+                    <div class="flex flex-col gap-4 p-4 rounded-lg bg-white drop-shadow-lg">
+                        <span>
+                            <img src="#" alt="karir-1" height="50%" width="50%" >
+                        </span>
+                        <span>
+                            <p class="font-bold text-lg">Matematic for 3 SMP</p>
+                            <p class="text-sm">09.00 - 12.00</p>
+                            <p class="text-sm">Rp. 750.000,00-/Month</p>
+                        </span>
+                        <span class="flex flex-row-reverse gap-4">
+                            <button class="px-4 py-2 rounded-lg bg-cyan-500 text-white drop-shadow-md">
+                                Apply Job
+                            </button>
+                            <button class="px-4 py-2 rounded-lg bg-white drop-shadow-md">
+                                Detail Job
+                            </button>
                         </span>
                     </div>
                 </div>
+
                 <div class="h-full">
 
 
