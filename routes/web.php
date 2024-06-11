6388d86488bf1a5e230e087ebcaa89adc009b06c
unknown <wahyujrs191003@gmail.com>
2024-06-11 14:48:20 +0700

    menambahkan routes yang hilang

--- routes/web.php
+++ routes/web.php
@@ -62,53 +62,125 @@ Route::get('/jobseekerpassword', function () {
 
 // Parents
 
-Route::get('/dashboard-parents', function(){
+Route::get('/dashboard-parents', function () {
     return view('/parents/dashboard-parents');
 });
-Route::get('/tutor-applicants-parents', function(){
+Route::get('/tutor-applicants-parents', function () {
     return view('/parents/tutor-applicants-parent');
 });
-Route::get('/find-tutor-parent', function(){
+Route::get('/find-tutor-parent', function () {
     return view('/parents/find-tutor-parent');
 });
-Route::get('/tutor-review-parent', function(){
+Route::get('/tutor-review-parent', function () {
     return view('/parents/tutor-review-parent');
 });
-Route::get('/tutor-review-parent-tambahlowongan', function(){
+Route::get('/tutor-review-parent-tambahlowongan', function () {
     return view('/parents/find-tutor-parent-form');
 });
 
 //operator
-Route::get('/operator-tutor-review', function(){
+Route::get('/operator-tutor-review', function () {
     return view('/operator/operator-tutor-review');
 });
 
-Route::get('/operator-tutor-review-detail', function(){
+Route::get('/operator-tutor-review-detail', function () {
     return view('/operator/operator-tutor-review-detail');
 });
 
-Route::get('/operator-tutor-status', function(){
+Route::get('/operator-tutor-status', function () {
     return view('/operator/operator-tutor-status');
 });
 
-Route::get('/operator-tutor-status-detail', function(){
+Route::get('/operator-tutor-status-detail', function () {
     return view('/operator/operator-tutor-status-detail');
 });
 
-Route::get('/operator-tutor-criteria-inbox', function(){
+Route::get('/operator-tutor-criteria-inbox', function () {
     return view('/operator/operator-tutor-criteria-inbox');
 });
 
-Route::get('/operator-tutor-criteria-inbox-detail', function(){
+Route::get('/operator-tutor-criteria-inbox-detail', function () {
     return view('/operator/operator-tutor-criteria-inbox-detail');
 });
 
-Route::get('/operator-payment-history', function(){
+Route::get('/operator-payment-history', function () {
     return view('/operator/operator-payment-history');
 });
 
-Route::get('/operator-payment', function(){
+Route::get('/operator-payment', function () {
     return view('/operator/operator-payment');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/audit', function () {
+    return view('/superadmin/audit-logs');
+});
+
+Route::get('/superadmin-dashboard', function () {
+    return view('/superadmin/dashboard');
+});
+
+Route::get('/paymentparent', function () {
+    return view('/superadmin/payment-from-parent');
+});
+
+Route::get('/paymentoperator', function () {
+    return view('/superadmin/payment-for-operator');
+});
+
+Route::get('/paymentoperatorhistory', function () {
+    return view('/superadmin/payment-for-operator-history');
+});
+
+Route::get('/paymentoperatordetail', function () {
+    return view('/superadmin/payment-for-operator-detail');
+});
+
+Route::get('/tutorlist', function () {
+    return view('/superadmin/tutor-list');
+});
+
+Route::get('/tutorlistdetail', function () {
+    return view('/superadmin/tutor-list-detail');
+});
+
+Route::get('/tutorreview', function () {
+    return view('/superadmin/tutor-review');
+});
+
+Route::get('/tutorreviewdetail', function () {
+    return view('/superadmin/tutor-review-detail');
+});
+
+Route::get('/tutorcriteriainbox', function () {
+    return view('/superadmin/tutor-criteria-inbox');
+});
+
+Route::get('/tutorcriteriainboxdetail', function () {
+    return view('/superadmin/tutor-criteria-inbox-detail');
+});
+
+Route::get('/tutordashboard', function () {
+    return view('/tutor/tutor-dashboard');
+});
+
+Route::get('/tutorschedule', function () {
+    return view('/tutor/tutor-schedule');
+});
+
+Route::get('/tutorpayment', function () {
+    return view('/tutor/tutor-payment');
+});
+
+Route::get('/tutorprofile', function () {
+    return view('/tutor/tutor-profile');
+});
+
+Route::get('/tutorprofileedit', function () {
+    return view('/tutor/tutor-profile-edit');
 });
 
 
@@ -116,6 +188,5 @@ Route::get('/operator-payment', function(){
 
 
 
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
